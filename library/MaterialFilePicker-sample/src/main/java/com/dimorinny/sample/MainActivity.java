8e2e0c9d35fe292fb0f6e06256462c27298f7395
Warren R. Bank <warren.r.bank@gmail.com>
2019-06-02 17:31:09 -0700

    fork: migrate support libraries to androidx

--- library/MaterialFilePicker-sample/src/main/java/com/dimorinny/sample/MainActivity.java
+++ library/MaterialFilePicker-sample/src/main/java/com/dimorinny/sample/MainActivity.java
@@ -2,11 +2,11 @@ package com.dimorinny.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
--- library/MaterialFilePicker-sample/src/main/java/com/dimorinny/sample/SampleFragment.java
+++ library/MaterialFilePicker-sample/src/main/java/com/dimorinny/sample/SampleFragment.java
@@ -3,13 +3,13 @@ package com.dimorinny.sample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.fragment.app.Fragment;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
